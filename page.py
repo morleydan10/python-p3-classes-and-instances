37872796539cc001f4567d2178a6d03086e1f270
Daniel Morley <morleydan10@gmail.com>
2023-12-10 03:23:44 -0500

    code-along

---
+++ page.py
@@ -0,0 +1,12 @@
+class Dog:
+    pass
+
+fido = Dog()
+fido
+# Fido, Snoopy, Lassie are two variables pointing at two separate instances of the same class
+snoopy = Dog()
+snoopy
+
+lassie = Dog()
+lassie
+
